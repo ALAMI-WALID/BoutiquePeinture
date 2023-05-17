934b10e73c0c728a9dc027bc14580b018eeb7870
walid Alami <walidalami1998@gmail.com>
2023-05-17 22:10:33 +0200

    register et l'auth

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,26 +2,34 @@
 
 namespace App\Controller;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class RegisterController extends AbstractController
 {
+
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {   
         $user = new User();
         $form = $this->createForm(RegisterType::class,$user);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid() ){
             $user=$form->getData();
-            $doctrine = $this->getDoctrine()->getManager();
-            $doctrine->persist($user);
-            $doctrine->flush();
+            $password = $encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($password);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
         }
         
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,10 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterType extends AbstractType
 {
@@ -18,35 +21,54 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre Prénom',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 3,
+                                'max' => 30])],
                 'attr'=> [
                     'placeholder'=> 'Merci de saisir votre Prénom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre Nom',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 3,
+                                'max' => 30])],
                 'attr' => [
                     'placeholder'=> 'Merci de saisir votre Nom'
                 ]
             ])
             ->add('email',EmailType::class, [
                 'label' => 'Votre Email',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 3,
+                                'max' => 30])],
                 'attr' => [
                     'placeholder'=> 'Merci de saisir votre Email'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'label' => 'Votre mot de passe',
-                'attr' => [
-                    'placeholder'=> 'Merci de saisir un mot de pass'
-                ]
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Entrz votre mot de passe',
+                    'attr' => [
+                        'placeholder'=> 'Merci de saisir votre Email'
+                    ]
+            ],
+                'second_options' =>[
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder'=> 'Merci de confirmer votre Email'
+                    ]
+                ],
+                'invalid_message' => 'le mot de passe et la confirmation doivent être identique.',
+
             ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmez votre mot de passe',
-                'mapped'=>false,
-                'attr' => [
-                    'placeholder'=> 'Confirmez votre mot de passe'
-                ]
-            ])
+           
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire",
                 
