44c211aedf730f143589cb71fa74dca8b919fd0a
walid Alami <walidalami1998@gmail.com>
2023-06-08 05:03:34 +0200

    mise a jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +11,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-       
+
         return $this->render('home/index.html.twig');
     }
 }
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -13,28 +13,43 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
-
     private $entityManager;
+
     public function __construct(EntityManagerInterface $entityManager){
         $this->entityManager = $entityManager;
     }
     #[Route('/inscription', name: 'register')]
     public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {   
+        $notification = null;
         $user = new User();
         $form = $this->createForm(RegisterType::class,$user);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid() ){
             $user=$form->getData();
+            $search_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+            if (!$search_email) {
+
+
+
             $password = $encoder->encodePassword($user,$user->getPassword());
             $user->setPassword($password);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
+            $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.";
+
+
+            }else{
+                $notification = "L'email que vous avez renseigné existe déjà.";
+            }
 
         }
 
         return $this->render('register/index.html.twig',[
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'notification' =>$notification
+
+
         ]);
     }
 }
