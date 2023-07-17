dc147aaec68d7f648d590b2864ccadf811e8882a
walid Alami <walidalami1998@gmail.com>
2023-07-17 23:02:20 +0200

    admin header

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Carrier;
 use App\Entity\Category;
+use App\Entity\Header;
 use App\Entity\Order;
 use App\Entity\Product;
 use App\Entity\SousCategory;
@@ -60,6 +61,8 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Sous-Sous-Categories', 'fa fa-list', SousSousCategory::class);
         yield MenuItem::linkToCrud('Produits', 'fa fa-tag', Product::class);
         yield MenuItem::linkToCrud('Carriers', 'fa fa-truck', Carrier::class);
+        yield MenuItem::linkToCrud('Headers', 'fa fa-desktop', Header::class);
+
 
 
 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Header;
 
 class HomeController extends AbstractController
 {
@@ -21,11 +22,11 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
-        // $headers = $this->entityManager->getRepository(Header::class)->findAll();
+        $headers = $this->entityManager->getRepository(Header::class)->findAll();
 
         return $this->render('home/index.html.twig',[
             'products' => $products,
-            // 'headers' => $headers
+            'headers' => $headers
 
         ]);
     }
