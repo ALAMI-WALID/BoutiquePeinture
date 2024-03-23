19ac5cfdc56edd0afab35242bb3dd7839606d813
walid Alami <walidalami1998@gmail.com>
2024-03-23 11:02:25 +0100

    'end cookies'

---
+++ public/assets/js/cookies.js
@@ -0,0 +1,11 @@
+
+window.axeptioSettings = {
+  clientId: "65fe95faa7656704435d26f3",
+  cookiesVersion: "peintureautoexpert-fr-EU",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
